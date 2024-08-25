ba8949f162aac6574477f1bd4d0b7a1a07cda707
luist <luistrigueiros@gmail.com>
2024-08-25 16:10:47 +0100

    Incremental save

--- src/main/java/org/example/ExclamationFunction.java
+++ src/main/java/org/example/ExclamationFunction.java
@@ -1,11 +1,14 @@
 package org.example;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.pulsar.functions.api.Context;
 import org.apache.pulsar.functions.api.Function;
 
+@Slf4j
 public class ExclamationFunction implements Function<String, String> {
     @Override
     public String process(String s, Context context) throws Exception {
+        log.info("Processing {}", s);
         return s + "!";
     }
 }
